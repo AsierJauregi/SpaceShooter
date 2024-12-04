37c47f63f3a8ef91176ec19eae92ad61c9ae2e7a
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2024-12-04 18:09:42 +0100

    Player shooting logic added
    Shot logic + PlayerShooting logic script + PlayerShot Prefab added

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject shotPrefab;
+    [SerializeField] private float cooldown;
+    [SerializeField] private GameObject upperSpawpoint;
+    [SerializeField] private GameObject lowerSpawpoint;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        timer += 1 * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space) && timer > cooldown)
+        {
+            Instantiate(shotPrefab, upperSpawpoint.transform.position, Quaternion.identity);
+            Instantiate(shotPrefab, lowerSpawpoint.transform.position, Quaternion.identity);
+            timer = 0;
+        }
+    }
+}
