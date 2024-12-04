7253d6bd77225e4634a4ce01c59ac478e81b844d
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2024-12-04 16:40:29 +0100

    Player and PlayerMovement Script added
    Player with movement logic added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxisRaw("Horizontal");
+        float inputV = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(new Vector2(inputH, inputV).normalized * speed * Time.deltaTime);
+        ClampPosition();
+    }
+
+    private void ClampPosition()
+    {
+        float xClamped = Mathf.Clamp(transform.position.x, -8.24f, 8.24f);
+        float yClamped = Mathf.Clamp(transform.position.y, -4.53f, 4.53f);
+        transform.position = new Vector3(xClamped, yClamped, 0);
+    }
+}
