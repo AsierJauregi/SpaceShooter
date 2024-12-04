e29785b21ac4d3040f2a9136bf6418915f35642c
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2024-12-04 17:13:15 +0100

    Movement methods refactored and renamed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,14 +15,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        DelimitMovement();
+    }
+
+    private void Move()
+    {
         float inputH = Input.GetAxisRaw("Horizontal");
         float inputV = Input.GetAxisRaw("Vertical");
 
         transform.Translate(new Vector2(inputH, inputV).normalized * speed * Time.deltaTime);
-        ClampPosition();
     }
 
-    private void ClampPosition()
+    private void DelimitMovement()
     {
         float xClamped = Mathf.Clamp(transform.position.x, -8.24f, 8.24f);
         float yClamped = Mathf.Clamp(transform.position.y, -4.53f, 4.53f);
