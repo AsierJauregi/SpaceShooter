c164661bc79138c8ee11daf4ef4c86207ae1b161
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2024-12-06 15:06:39 +0100

    Bug taking double the damage fixed + PLayer interaction with Powerups implemented in PlayerLogic

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -1,20 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerLogic : MonoBehaviour
 {
-    [SerializeField] private string tagEnemy = "Enemy";
+    [SerializeField] private string shieldPowerUpTag = "ShieldPowerUp";
+    [SerializeField] private string shotPowerUpTag = "ShotPowerUp";
+    [SerializeField] private string healingPowerUpTag = "HealingPowerUp";
+    [SerializeField] private string enemyTag = "Enemy";
+    [SerializeField] private string shotEnemyTag = "ShotEnemy";
     [SerializeField] private float speed;
-    [SerializeField] private string tag = "ShotEnemy";
-    [SerializeField] private float health;
+    private float health;
+    [SerializeField] private float maxHealth;
     [SerializeField] private TextMeshProUGUI textHealth;
-
+    [SerializeField] private GameObject shieldGameObject;
+    [SerializeField] private float powerUpDuration;
+    [SerializeField] private bool hasShieldPowerUp;
+    [SerializeField] private bool hasShotPowerUp;
+    [SerializeField] private bool isTakingDamage = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHealth;
+        shieldGameObject.GetComponent<SpriteRenderer>().enabled = false;
     }
 
     // Update is called once per frame
@@ -38,18 +49,81 @@ public class PlayerLogic : MonoBehaviour
         float yClamped = Mathf.Clamp(transform.position.y, -4.53f, 4.53f);
         transform.position = new Vector3(xClamped, yClamped, 0);
     }
+
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag(tag) || collision.gameObject.CompareTag(tagEnemy))
+        if (collision.gameObject.CompareTag(shotEnemyTag) || collision.gameObject.CompareTag(enemyTag))
         {
-            health -= 20;
-            Debug.Log("Helth: " + health);
             Destroy(collision.gameObject);
-            if(health <= 0)
+            TakeDamage();
+        }
+        else if (collision.gameObject.CompareTag(healingPowerUpTag))
+        {
+            Destroy(collision.gameObject);
+            Heal();
+        }
+        else if (collision.gameObject.CompareTag(shotPowerUpTag))
+        {
+            Destroy(collision.gameObject);
+            StartCoroutine(BoostShot());
+        }
+        else if (collision.gameObject.CompareTag(shieldPowerUpTag))
+        {
+            Destroy(collision.gameObject);
+            StartCoroutine(ActivateShield());
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(shotEnemyTag) || collision.gameObject.CompareTag(enemyTag))
+        {
+            isTakingDamage = false;
+        }
+    }
+    IEnumerator ActivateShield()
+    {
+        hasShieldPowerUp = true;
+        shieldGameObject.GetComponent<SpriteRenderer>().enabled = true;
+
+        yield return new WaitForSeconds(powerUpDuration);
+
+        hasShieldPowerUp = false;
+        shieldGameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    IEnumerator BoostShot()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+    }
+
+    private void Heal()
+        {
+            if(health < maxHealth)
             {
-                Debug.Log("GAME OVER");
-                Destroy(this.gameObject);
+                health += 20;
+                Debug.Log("HEAL!! Health: " + health);
             }
+        }
+    private void TakeDamage()
+    {
+
+        if (!hasShieldPowerUp && !isTakingDamage)
+        {
+            isTakingDamage = true;
+            health -= 20;
+            Debug.Log("Health: " + health);
+        }
+        else if(hasShieldPowerUp) 
+        {
+            Debug.Log("Protected by shield!");
+        }
+        
+        
+        if (health <= 0)
+        {
+            Debug.Log("GAME OVER");
+            Destroy(this.gameObject);
         }
     }
 }
